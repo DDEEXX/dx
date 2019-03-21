d3ae13d9a221f9b5fb254d260820a2d5c08c221f
DEX_server <dex@meleuz.ru>
2019-03-21 17:21:39 +0500

    move hall new

--- scripts/move_holl.php
+++ scripts/move_holl.php
@@ -9,10 +9,10 @@
 // проверить!!! Если день и ключить через сайт, то при движениитут же гаснет.
 // Если ночью включить через сайт, никогда не гаснет
 
-require_once(dirname(__FILE__).'/../class/globalConst.interface.php');
-require_once(dirname(__FILE__).'/../class/lists.class.php');
-require_once(dirname(__FILE__).'/../class/managerUnits.class.php');
-require_once(dirname(__FILE__).'/../class/sunInfo.class.php');
+require_once(dirname(__FILE__) . '/../class/globalConst.interface.php');
+require_once(dirname(__FILE__) . '/../class/lists.class.php');
+require_once(dirname(__FILE__) . '/../class/managerUnits.class.php');
+require_once(dirname(__FILE__) . '/../class/sunInfo.class.php');
 
 $NAME_MOVE = 'move_1';
 $NAME_LIGHT_N = 'light_hol_2_n';
@@ -23,66 +23,96 @@ $unitNightLight = managerUnits::getUnitLabel($NAME_LIGHT_N);
 
 if (is_null($unitMove) || is_null($unitNightLight)) return;
 
-    //Есть движение
-    $isMove = $unitMove->getValue();
+//Есть движение
+$isMove = $unitMove->getValue();
 
-    //Обновляем в БД значение датчика движения
-    $unitMove->updateStatus($isMove);
+//Обновляем в БД значение датчика движения
+$unitMove->updateStatus($isMove);
 
-    //Свет горит
-    $isLight = $unitNightLight->getValue(); //
+//Свет горит
+$isLight = $unitNightLight->getValue(); //
 
-    //Каким образом включилася подсветка или выключена
-    $statusKey = $unitNightLight->readLastStatusKeyJournal();
+//Каким образом включилася подсветка или выключена
+$statusKey = $unitNightLight->readLastStatusKeyJournal();
 
-    //Часть дня ночь/утро/день/вечер
-    $sunInfo = sunInfo::getSunInfo(mktime());
+//Часть дня ночь/утро/день/вечер
+$sunInfo = sunInfo::getSunInfo(mktime());
 
-    //Время когда последний раз отключился датчик движения
-    $timeNoMove = $unitMove->readWhereLastStatus(0);
+//Время когда последний раз отключился датчик движения
+$timeNoMove = $unitMove->readWhereLastStatus(0);
 
-    if ($timeNoMove===false || is_null($timeNoMove)) {
-        $moveTime = 99999;
+if ($timeNoMove === false || is_null($timeNoMove)) {
+    $moveTime = 99999;
+}
+else {
+    $moveTime = time() - strtotime($timeNoMove);
+}
+
+if ($sunInfo = dayPart::NIGHT) { // ночь
+    if ($isMove) { // есть движение
+        if (!$isLight) { // свет не горит
+            $unitNightLight->setValue(1, statusKey::MOVE); // включает, записываем что от датчика
+        }
     }
-    else {
-        $moveTime = time()-strtotime($timeNoMove);
-    }
-
-    if ($isMove) {
-        if ($isLight) {
-            if ($sunInfo!=dayPart::NIGHT) {
-                if ($moveTime>$MOVE_TIME_N) {
-                    if ($statusKey==statusKey::MOVE) {
-                        $unitNightLight->setValue(0, statusKey::OFF);
-                    }
+    else { // нет движения
+        if ($isLight) { // горит свет
+            if ($statusKey == statusKey::MOVE) { // включился датчиком движения
+                if ($moveTime > $MOVE_TIME_N) { // время вышло с последнего отсутствия движения
+                    $unitNightLight->setValue(0, statusKey::OFF); //гасим
+                }
+            }
+            else { // свет включили вручную (через сайт)
+                // это для проверки
+                if ($moveTime > $MOVE_TIME_N) { // время вышло с последнего отсутствия движения
+                    $unitNightLight->setValue(0, statusKey::OFF); //гасим
                 }
             }
         }
-        else {
-            if ($sunInfo==dayPart::NIGHT) {
-                $unitNightLight->setValue(1, statusKey::MOVE);
-            }
+    }
+}
+else { // светло
+    if ($isLight) { // горит свет
+        if ($statusKey == statusKey::MOVE) { // включился датчиком движения
+            $unitNightLight->setValue(0, statusKey::OFF); //гасим
         }
     }
-    else {
-        if ($isLight) {
-            if ($statusKey == statusKey::MOVE) { //включился от датчика движения
-                if ($sunInfo==dayPart::NIGHT) {
-                    if ($moveTime>$MOVE_TIME_N) {
-                        $unitNightLight->setValue(0, statusKey::OFF);
-                    }
-                }
-                else {
-                    $unitNightLight->setValue(0, statusKey::OFF);
-                }
-            }
-            else {
-                if ($moveTime>$MOVE_TIME_N) {
-                    $unitNightLight->setValue(0, statusKey::OFF);
-                }
-            }
-        }
-    };
+}
+
+//if ($isMove) {
+//    if ($isLight) {
+//        if ($sunInfo != dayPart::NIGHT) {
+//            if ($moveTime > $MOVE_TIME_N) {
+//                if ($statusKey == statusKey::MOVE) {
+//                    $unitNightLight->setValue(0, statusKey::OFF);
+//                }
+//            }
+//        }
+//    }
+//    else {
+//        if ($sunInfo == dayPart::NIGHT) {
+//            $unitNightLight->setValue(1, statusKey::MOVE);
+//        }
+//    }
+//}
+//else {
+//    if ($isLight) {
+//        if ($statusKey == statusKey::MOVE) { //включился от датчика движения
+//            if ($sunInfo == dayPart::NIGHT) {
+//                if ($moveTime > $MOVE_TIME_N) {
+//                    $unitNightLight->setValue(0, statusKey::OFF);
+//                }
+//            }
+//            else {
+//                $unitNightLight->setValue(0, statusKey::OFF);
+//            }
+//        }
+//        else {
+//            if ($moveTime > $MOVE_TIME_N) {
+//                $unitNightLight->setValue(0, statusKey::OFF);
+//            }
+//        }
+//    }
+//};
 
 unset($unitMove);
 unset($unitNightLight);
