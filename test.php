dec8d1dd90fcfdc274b4401ca4453388d9cd0479
DEX_server <dex@meleuz.ru>
2019-05-23 12:51:42 +0500

    test sensors

--- test.php
+++ test.php
@@ -8,7 +8,26 @@
 
 require_once(dirname(__FILE__) . '/class/globalConst.interface.php');
 require_once(dirname(__FILE__) . '/class/lists.class.php');
+require_once(dirname(__FILE__) . '/class/logger.class.php');
 require_once(dirname(__FILE__) . '/class/managerUnits.class.php');
+
+$sel = new selectOption();
+$sel->set('SensorTypeID', typeDevice::TEMPERATURE);
+$sel->set('Disabled', 0);
+
+$temperatureUnits = managerUnits::getListUnits($sel);
+
+foreach ($temperatureUnits as $tekUnit) {
+    logger::writeLog('опрос датчика температуры ID='.$tekUnit->getId());
+    $val = $tekUnit->getValue();
+    logger::writeLog('значение val='.var_dump($val));
+    if (!is_null($val)) {
+        logger::writeLog('запись в базу');
+        $tekUnit->writeValue($val);
+    }
+}
+
+unset($temperatureUnits);
 
 $sel = new selectOption();
 $sel->set('SensorTypeID', typeDevice::PRESSURE);
@@ -17,7 +36,31 @@ $sel->set('Disabled', 0);
 $pressureUnits = managerUnits::getListUnits($sel);
 
 foreach ($pressureUnits as $tekUnit) {
-    $tekUnit->getAverageForInterval(10);
+    logger::writeLog('опрос датчика давления ID='.$tekUnit->getId());
+    $val = $tekUnit->getValue();
+    logger::writeLog('значение val='.var_dump($val));
+    if (!is_null($val)) {
+        logger::writeLog('запись в базу');
+        $tekUnit->writeValue($val);
+    }
 }
 
 unset($pressureUnits);
+
+$sel = new selectOption();
+$sel->set('SensorTypeID', typeDevice::HUMIDITY);
+$sel->set('Disabled', 0);
+
+$humidityUnits = managerUnits::getListUnits($sel);
+
+foreach ($humidityUnits as $tekUnit) {
+    logger::writeLog('опрос датчика влажности ID='.$tekUnit->getId());
+    $val = $tekUnit->getValue();
+    logger::writeLog('значение val='.var_dump($val));
+    if (!is_null($val)) {
+        logger::writeLog('запись в базу');
+        $tekUnit->writeValue($val);
+    }
+}
+
+unset($humidityUnits);
