3e32dc770fa549d2783da32c062592dc42915ed0
DEX <dddeeexxx@mail.ru>
2020-11-24 18:52:09 +0500

    dx 014

--- runScripts.php
+++ runScripts.php
@@ -6,20 +6,30 @@
  * Time: 22:45
  */
 
-//sleep(15);
+$fileDir = dirname(__FILE__);
 
-$arr_s = array('scripts/move_hall.php');
+include_once ($fileDir.'/class/daemonScripts.class.php');
 
-$loop = true;
-
-while ($loop) {
-
-    usleep(1000);
-
-    for ($i = 0; $i < count($arr_s); $i++) {
-        $pathScript = dirname(__FILE__) . '/' . $arr_s[$i];
-        /** @noinspection PhpIncludeInspection */
-        include($pathScript);
-    }
-
+ //Создаем дочерний процесс весь код после pcntl_fork() будет выполняться двумя процессами: родительским и дочерним
+$child_pid = pcntl_fork();
+if ($child_pid) { // Выходим из родительского, привязанного к консоли, процесса
+    exit();
 }
+// Делаем основным процессом дочерний.
+posix_setsid();
+// Дальнейший код выполнится только дочерним процессом, который уже отвязан от консоли
+
+ini_set('error_log',$fileDir.'/logs/error.log');
+fclose(STDIN);
+fclose(STDOUT);
+fclose(STDERR);
+$STDIN = fopen('/dev/null', 'r');
+$STDOUT = fopen($fileDir.'/logs/application.log', 'ab');
+$STDERR = fopen($fileDir.'/logs/daemonRunScript.log', 'ab');
+
+$daemon = new daemonScripts($fileDir.'/scripts', $fileDir.'/tmp');
+if ($daemon->isDaemonActive()) {
+    exit();
+}
+$daemon->putPitFile(getmypid());
+$daemon->run();
