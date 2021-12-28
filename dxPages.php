a9ef433c0f8a2aa5a744a686ade92f29c7facce2
root <dddeeexxx@mail.ru>
2021-12-28 11:28:50 +0500

    background temp

--- dxPages.php
+++ dxPages.php
@@ -436,13 +436,13 @@ if ($p == 'heater') {
                     <div class="ui-corner-all ui-state-default ui-widget-content"
                          style="height: 500px;margin-top:5px;position:relative">
                         <div id="floor1"><img src="img2/home_.png"></div>
-                        <div id="temp_hall" class="ui-corner-all ui-state-default ui-widget-content temp_plan"
+                        <div id="temp_hall" class="temp_plan ui-corner-all ui-state-default ui-widget-content"
                              style="position:absolute;left:100px;top:220px"></div>
                         <div id="temp_bedroom" class="temp_plan ui-corner-all ui-state-default ui-widget-content"
                              style="position:absolute;left:540px;top:310px"></div>
                         <div id="temp_bathroom" class="temp_plan ui-corner-all ui-state-default ui-widget-content"
                              style="position:absolute;left:750px;top:260px"></div>
-                        <div id="temp_bedroom_Lera" class="ui-corner-all ui-state-default ui-widget-content"
+                        <div id="temp_bedroom_Lera" class="temp_plan ui-corner-all ui-state-default ui-widget-content"
                              style="position:absolute;left:600px;top:190px"></div>
                     </div>
                 </div>
