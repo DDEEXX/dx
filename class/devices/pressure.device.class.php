468b1d69cad016d09138b923b5b809f9f08551f7
root <dddeeexxx@mail.ru>
2023-10-17 17:39:52 +0500

    fix

--- class/devices/pressure.device.class.php
+++ class/devices/pressure.device.class.php
@@ -2,24 +2,86 @@
 /** Датчик атмосферного давления
  */
 
+class formatterPressureMQTT_1 implements iFormatterValue
+{
+    function formatRawValue($value)
+    {
+        $result = new formatDeviceValue();
+        $result->valueNull = false;
+        $result->status = 0;
+        $arValueData = json_decode($value, true);
+        if (is_numeric($arValueData['pressure']))
+            $result->value = $arValueData['pressure'];
+        else {
+            $result->value = '';
+            $result->valueNull = true;
+        }
+        return $result;
+    }
+
+    function formatTestCode($value)
+    {
+        $objValue = json_decode($value);
+        switch ($objValue->state) {
+            case 'online' :
+                $testCode = testDeviceCode::WORKING;
+                break;
+            case 'offline' :
+                $testCode = testDeviceCode::NO_CONNECTION;
+                break;
+            default :
+                $testCode = testDeviceCode::UNKNOWN;
+        }
+        return $testCode;
+    }
+
+    function formatOutData($data)
+    {
+        return $data;
+    }
+}
+
+
 class pressureSensorMQQTPhysic extends aDeviceSensorPhysicMQTT
 {
-    const DEFAULT_PAYLOAD = 'pressure';
-
-    public function __construct($mqttParameters)
+    private static function getConstructParam($parameters, &$mqttParameters)
     {
-        if (empty($mqttParameters['payloadRequest'])) $mqttParameters['payloadRequest'] = self::DEFAULT_PAYLOAD;
+        $result = [];
+        $result['payloadRequest'] = '';
+        $result['selfState'] = false;
+        $result['formatter'] = null;
+        switch ($parameters['valueFormat']) {
+            case 0 :
+                $mqttParameters['payloadRequest'] = 'pressure';
+                $result['selfState'] = false;
+                $result['formatter'] = new formatterNumeric();
+                break;
+            case 1 :
+                $mqttParameters['payloadRequest'] = '{"state": ""}';
+                $mqttParameters['topicAvailability'] = '';
+                $result['selfState'] = true;
+                $result['formatter'] = new formatterPressureMQTT_1();
+                break;
+        }
+        return $result;
+    }
+
+    public function __construct($parameters, $mqttParameters)
+    {
+        $param = self::getConstructParam($parameters, $mqttParameters);
+        $this->selfState = $param['selfState'];
+        $this->value = valuesFactory::createDeviceValue($parameters, $param['formatter']);
         parent::__construct($mqttParameters, formatValueDevice::MQTT_PRESSURE);
     }
 }
 
 class pressureSensorFactory
 {
-    static public function create($net, $mqttParameters)
+    static public function create($parameters, $mqttParameters)
     {
-        switch ($net) {
+        switch ($parameters['net']) {
             case netDevice::ETHERNET_MQTT:
-                return new pressureSensorMQQTPhysic($mqttParameters);
+                return new pressureSensorMQQTPhysic($parameters, $mqttParameters);
             default :
                 return new DeviceSensorPhysicDefault();
         }
@@ -32,13 +94,20 @@ class pressureSensorDevice extends aSensorDevice
     public function __construct(array $options)
     {
         parent::__construct($options, typeDevice::PRESSURE);
+        parent::__construct($options, typeDevice::HUMIDITY);
+        $parameters = [
+            'deviceID' => $this->getDeviceID(),
+            'net' => $this->getNet(),
+            'valueFormat' => $options['value_format'],
+            'valueStorage' => $options['value_storage']
+        ];
         $mqttParameters = [
             'topicCmnd' => $options['topic_cmnd'],
             'topicStat' => $options['topic_stat'],
             'topicTest' => $options['topic_test'],
             'topicAlarm' => $options['topic_alarm'],
             'payloadRequest' => $options['payload_cmnd']];
-        $this->devicePhysic = pressureSensorFactory::create($this->getNet(), $mqttParameters);
+        $this->devicePhysic = pressureSensorFactory::create($parameters, $mqttParameters);
     }
 
     function requestData()
