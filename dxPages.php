3fbf6b58948b1761f9de2336d23b61cb752742bf
root <dddeeexxx@mail.ru>
2023-10-20 18:25:27 +0500

    fix

--- dxPages.php
+++ dxPages.php
@@ -156,7 +156,7 @@ if ($p == 'home' || empty($p)) {
     <div id="page_home" class="grid_12" style="height: 790px">
         <div class="grid_12" style="height: 70px">
             <div class="grid_9 alpha">1
-                <button id="button_full_screen"></button>
+                <button id="button_full_screen" style="height: 35px; width: 35px"></button>
             </div>
             <div class="grid_2 omega">
                 <div id="TekDate" class="TekDate" style="font-size:110%"></div>
