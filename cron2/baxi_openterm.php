949f5793b003c16ce937b95715e37b7389f5017d
root <dddeeexxx@mail.ru>
2023-10-22 20:32:08 +0500

    fix

--- cron2/baxi_openterm.php
+++ cron2/baxi_openterm.php
@@ -2,11 +2,12 @@
 require_once(dirname(__FILE__) . '/../class/managerUnits.class.php');
 require_once(dirname(__FILE__) . '/../class/mqtt.class.php');
 
+$mqtt = mqttSend::connect();
+
 $unitTemperatureOut = managerUnits::getUnitLabel('temp_out_2');
 $temperatureOut = $unitTemperatureOut->getData();
 $dateValue = $temperatureOut->date;
 if (!is_null($temperatureOut->valueNull) && time() - $dateValue < 1200) {
-    $mqtt = mqttSend::connect();
     $topic = 'baxi_open/controller/set';
     $payload = sprintf('{"eout":%s}', $temperatureOut->value);
     $mqtt->publish($topic, $payload);
@@ -17,7 +18,6 @@ $unitTemperatureIn = managerUnits::getUnitLabel('temp_bedroom');
 $temperatureIn = $unitTemperatureIn->getData();
 $dateValue = $temperatureIn->date;
 if (!is_null($temperatureIn->valueNull) && time() - $dateValue < 1200) {
-    $mqtt = mqttSend::connect();
     $topic = 'baxi_open/controller/set';
     $payload = sprintf('{"ein":%s}', $temperatureIn->value);
     $mqtt->publish($topic, $payload);
@@ -28,7 +28,6 @@ $temperatureBoilerIn = $unitTemperatureBoilerIn->getData();
 echo var_dump($temperatureBoilerIn);
 $dateValue = $temperatureBoilerIn->date;
 if (!is_null($temperatureBoilerIn->valueNull) && time() - $dateValue < 1200) {
-    $mqtt = mqttSend::connect();
     $topic = 'baxi_open/controller/set';
     $payload = sprintf('{"retb":%s}', $temperatureBoilerIn->value);
     $mqtt->publish($topic, $payload);
@@ -39,7 +38,6 @@ $pressure = $unitePressure->getData();
 echo var_dump($pressure);
 $dateValue = $pressure->date;
 if (!is_null($pressure->valueNull) && time() - $dateValue < 1200) {
-    $mqtt = mqttSend::connect();
     $topic = 'baxi_open/controller/set';
     $payload = sprintf('{"presb":%s}', $pressure->value);
     $mqtt->publish($topic, $payload);
