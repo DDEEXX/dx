b9d87c9ab60aefc9bde70c01def25475fd55e7d0
root <dddeeexxx@mail.ru>
2023-10-24 18:07:54 +0500

    fix

--- js2/heater.js
+++ js2/heater.js
@@ -14,86 +14,81 @@ function heater_updateDataScheme() {
 
 function heater_updateBoiler() {
     $.get('data/heater/heating.php?dev=boiler&label=boiler_opentherm', function (data) {
-        $('#boiler_ch').html( data.ch + " &degC" );
-        $('#boiler_retb').html( data.retb + " &degC" );
-        $('#boiler_tset').html( data.tset + " &degC" );
-        $('#boiler_dhw').html( data.dhw + " &degC" );
-        $('#boiler_spdhw').html( data.spdhw + " &degC" );
-        if (data.chon) {
-            if (data.flon) {
-                $('#boiler_heating_fire').attr('src', 'img2/icon_small/fire.png')
-                $('#boiler_heating_fire_level').html(data.mlev + '%');
-            } else {
-                $('#boiler_heating_fire').attr('src', 'img2/icon_small/fire_.png')
-                $('#boiler_heating_fire_level').html("");
-            }
+        $('#boiler_ch').html(data.ch + " &degC");
+        $('#boiler_retb').html(data.retb + " &degC");
+        $('#boiler_tset').html(data.tset + " &degC");
+        $('#boiler_dhw').html(data.dhw + " &degC");
+        $('#boiler_spdhw').html(data.spdhw + " &degC");
+        if (data.chon && data.flon) {
+            $('#boiler_heating_fire').attr('src', 'img2/icon_small/fire.png')
+            $('#boiler_heating_fire_level').html(data.mlev + '%');
+        } else {
+            $('#boiler_heating_fire').attr('src', 'img2/icon_small/fire_.png')
+            $('#boiler_heating_fire_level').html("");
         }
-        if (data.dhwon) {
-            if (data.flon) {
-                $('#boiler_heating_wfire').attr('src', 'img2/icon_small/fire.png')
-                $('#boiler_heating_wfire_level').html(data.mlev + '%');
-            } else {
-                $('#boiler_heating_wfire').attr('src', 'img2/icon_small/fire_.png')
-                $('#boiler_heating_wfire_level').html("");
-            }
+        if (data.dhwon && data.flon) {
+            $('#boiler_heating_wfire').attr('src', 'img2/icon_small/fire.png')
+            $('#boiler_heating_wfire_level').html(data.mlev + '%');
+        } else {
+            $('#boiler_heating_wfire').attr('src', 'img2/icon_small/fire_.png')
+            $('#boiler_heating_wfire_level').html("");
         }
-
     });
 }
 
 $(function () {
 
-    $( "#heater_boiler_heating" ).slider({
+    $("#heater_boiler_heating").slider({
         min: 190,
         max: 280,
-        create: function( event, ui ) {
+        create: function (event, ui) {
             let th = $(this);
             $.get('data/heater/heating.php?dev=boiler&label=boiler_opentherm', function (data) {
-                let spr10 = Math.round(data._spr*10);
-                th.slider( "value", spr10 );
-                $('#boiler_spr').html( spr10/10 + " &degC" );
+                let spr10 = Math.round(data._spr * 10);
+                th.slider("value", spr10);
+                $('#boiler_spr').html(spr10 / 10 + " &degC");
             });
         },
-        slide: function( event, ui ) {
-            $('#boiler_spr').html( ui.value/10 + " &degC" );
+        slide: function (event, ui) {
+            $('#boiler_spr').html(ui.value / 10 + " &degC");
         }
     });
 
-    $( "#heater_floor_1" ).slider({
+    $("#heater_floor_1").slider({
         min: 230,
         max: 400,
-        create: function( event, ui ) {
-            $(this).slider( "value", 290 );
-            $('#boiler_sprf1').html( 290/10 + " &degC" );
+        create: function (event, ui) {
+            $(this).slider("value", 290);
+            $('#boiler_sprf1').html(290 / 10 + " &degC");
         },
-        slide: function( event, ui ) {
-            $('#boiler_sprf1').html( ui.value/10 + " &degC" );
+        slide: function (event, ui) {
+            $('#boiler_sprf1').html(ui.value / 10 + " &degC");
         }
     });
-    $( "#heater_floor_bathroom" ).slider({
+    $("#heater_floor_bathroom").slider({
         min: 220,
         max: 280,
-        create: function( event, ui ) {
-            $(this).slider( "value", 240 );
-            $('#boiler_sprb').html( 240/10 + " &degC" );
+        create: function (event, ui) {
+            $(this).slider("value", 240);
+            $('#boiler_sprb').html(240 / 10 + " &degC");
         },
-        slide: function( event, ui ) {
-            $('#boiler_sprb').html( ui.value/10 + " &degC" );
+        slide: function (event, ui) {
+            $('#boiler_sprb').html(ui.value / 10 + " &degC");
         }
     });
-    $( "#heater_boiler_water" ).slider({
+    $("#heater_boiler_water").slider({
         min: 350,
         max: 600,
-        create: function( event, ui ) {
+        create: function (event, ui) {
             let th = $(this);
             $.get('data/heater/heating.php?dev=boiler&label=boiler_opentherm', function (data) {
-                let dhw10 = Math.round(data._dhw*10);
-                th.slider( "value", dhw10 );
-                $('#boiler_sprw').html( dhw10/10 + " &degC" );
+                let dhw10 = Math.round(data._dhw * 10);
+                th.slider("value", dhw10);
+                $('#boiler_sprw').html(dhw10 / 10 + " &degC");
             });
         },
-        slide: function( event, ui ) {
-            $('#boiler_sprw').html( ui.value/10 + " &degC" );
+        slide: function (event, ui) {
+            $('#boiler_sprw').html(ui.value / 10 + " &degC");
         }
     });
     heater_updateBoiler();
