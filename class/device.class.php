de478095b9a2cca2006b58b30319e509ebdc6c2e
root <dddeeexxx@mail.ru>
2023-11-01 17:06:38 +0500

    fix

--- class/device.class.php
+++ class/device.class.php
@@ -1299,6 +1299,7 @@ require_once dirname(__FILE__) . '/devices/zigbeeSwitchWHD02.device.class.php';
 require_once dirname(__FILE__) . '/devices/kitchenHood.device.class.php';
 require_once dirname(__FILE__) . '/devices/gasSensor.device.class.php';
 require_once dirname(__FILE__) . '/devices/boilerOpenTherm.class.php';
+require_once dirname(__FILE__) . '/devices/radiatorValve.class.php';
 
 class labelSensorDevice extends aSensorDevice
 {
