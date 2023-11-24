d1388dac9c24e0fae46d80cbd3ae650f9f2637c0
root <dddeeexxx@mail.ru>
2023-11-24 13:54:34 +0500

    fix auth

--- modules/getData.php
+++ modules/getData.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__) . '/class/managerUnits.class.php');
+require_once(dirname(__FILE__) . '/../class/managerUnits.class.php');
 
 const statusKeyData = ['', 'движение', 'web', '', 'неизвестно', 'модуль', 'датчик', 'сенсор'];
 
