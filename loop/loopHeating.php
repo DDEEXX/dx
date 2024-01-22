bbbc2efb2610af5bb1f2c5d7644de44d22675ba3
root <dddeeexxx@mail.ru>
2024-01-22 12:40:46 +0500

    heatting

--- loop/loopHeating.php
+++ loop/loopHeating.php
@@ -223,7 +223,7 @@ class daemonLoopHeating extends daemon
                         $log['f_val'] = $fCurValve;
                         $this->saveInJournal(json_encode($log), 'fl');
 
-                        $mss = 'f_op = '.$f_op.', $fCurValve = '.$fCurValve.', $fTarValve = '.$fTarValve.', f_val = '.$fCurValve;
+                        $mss = 'f_op = '.$f_op.', $floorTempCurrentLast = '.$floorTempCurrentLast.', $fCurValve = '.$fCurValve.', $fTarValve = '.$fTarValve.', f_val = '.$fCurValve;
                         logger::writeLog($mss, loggerTypeMessage::NOTICE, loggerName::DEBUG);
 
                     }
